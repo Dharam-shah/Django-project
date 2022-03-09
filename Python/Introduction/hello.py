48308da25d610ffc1e4b0cb3daaeb68781f2a71d
Dharam-shah <dharmendra11250@gmail.com>
2022-03-09 13:21:48 +0545

    Adding blog, portfolio and python files

---
+++ Python/Introduction/hello.py
@@ -0,0 +1,103 @@
+print("iam a python developer")
+
+a = 5  
+b = 2
+c = 3.14
+d = 'Python'
+e = "Hello, World"
+f = "1.5"
+g = 'Python'
+
+g = True  
+
+print(a) 
+print(e)
+print(g)
+
+
+# h = ''
+# print(h)
+
+number1 = input("Enter a first number: ")
+number2 = input("Enter a second number: ")
+#print("the number that you entered is : " , number1)
+print("the number that you entered is : ", number1 + ", "+ number2)
+#print(number)
+
+
+
+a = 1  #int
+b = 2.8 #float
+sum =  a + b  #float  1.0 + 2.8 = 3.8
+print(sum)
+
+# #b = '2.8'
+
+# print(type(sum))
+
+# int()
+# float()
+# str()
+
+# a = 1
+# b  = float(a)
+# print(b) #output: 1.0
+
+# print(type(b)) #output: <class 'float'>
+
+
+
+# c = 'hello'
+# print(c) #output: 100
+# print(type(c)) #output: <class 'str'>
+
+# d = int(c)
+# print(d) #output: 100
+# print(type(d)) #output: <class 'int'>
+
+#number operations
+a = 5
+b = 3
+
+# sum_ab = a + b
+# print(sum_ab) #output: 3.8
+
+# sub_ab = a - b
+# print(sub_ab) #output: -1.8
+
+# mult_ab = a * b
+# print(mult_ab) #output: 2.8
+
+div_ab = a / b
+#print(div_ab) #output: 0.5
+
+mod_ab = a % b
+#print(mod_ab) #output: 1
+
+floor_div_ab = a // b
+#print(floor_div_ab) #output: 0
+
+# exp_ab = a ** b
+# print(exp_ab) #output: 1
+
+# #string operations
+# #concatenation
+# a = 'Hello, '
+# b = 'world!'
+# c = a + b
+# print(c) #output: Hello,world!
+# print(type(c))
+
+# #multiplication
+# d = 'Hello, '
+# e = 'world!'
+# s = d + e
+# print(s * 3) #output: Hello, world!Hello, world!Hello, world!
+
+# a = 1
+# a //= 2  #a = a + 2
+# print(a) #output: 3
+
+# b = 1
+# b -= 2  #b = b - 2
+# print(b) #output: -1
