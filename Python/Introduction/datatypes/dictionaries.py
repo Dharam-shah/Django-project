28160353c15f793ac9f200298ae4c8c9908a95e3
Dharam-shah <dharmendra11250@gmail.com>
2022-03-14 15:41:16 +0545

    updating datatypes files

--- Python/Introduction/datatypes/dictionaries.py
+++ Python/Introduction/datatypes/dictionaries.py
@@ -1,60 +1,52 @@
-# #creating a dictionary
-# a = {}
-# b = {'one':1, 'two':2, 'three':[3,4,5]}
+#creating a dictionary
+a = {}
+b = {'one':1, 'two':2, 'three':[3,4,5]}
+print(b)
 
-# print(b)
+l = [(1,'python'),(3,'c++'),(2,'java')]
+d = dict(l)
+print(d)
 
-# l = [(1,'python'),(3,'c++'),(2,'java')]
-# d = dict(l)
-# print(d)
+person = {'name':'John', 'age':30, 'country':'Norway'}
+print(person['name'])
+print(person['age'])
+print(person['country'])
 
-# g  = 5
+name = person.get('name')
+age = person.get('age')
+country = person.get('country')
+print(name)
+print(age)
+print(country)
 
-# person = {'name':'John', 'age':30, 'country':'Norway'}
-# print(person['name'])
-# print(person['age'])
-# print(person['country'])
+#update, add items in a dictionary
+person['name'] = 'Jane'  #change
+person['age'] = 31
+person['country'] = 'USA'
+person['city'] = 'New York'  #add
+print(person)
 
-# name = person.get('name')
-# age = person.get('age')
-# country = person.get('country')
-# print(name)
-# print(age)
-# print(country)
+#delete items in a dictionary
+result = person.pop('city')
+print(result)
+print(person)
 
-# #update, add items in a dictionary
+del person['age']
+print(person)
 
-# person['name'] = 'Jane'  #change
-# person['age'] = 31
-# person['country'] = 'USA'
-# person['city'] = 'New York'  #add
-# print(person)
-
-
-# #delete items in a dictionary
-# result = person.pop('city')
-# print(result)
-# print(person)
-
-# del person['age']
-# print(person)
-
-# person.clear()
-# print(person)
+person.clear()
+print(person)
 
 #iterate through a dictionary
+numbers = {1:7, 2:8, 'three':3}
+for key in numbers:
+    print(key, numbers[key])
 
-# numbers = {1:7, 2:8, 'three':3}
-#numbers[1]
+for key,value in numbers.items():
+    print(key, value)
 
-# for key in numbers:
-#     print(key, numbers[key])
+for key in numbers.keys():
+    print(key)
 
-# for key,value in numbers.items():
-#     print(key, value)
-
-# for key in numbers.keys():
-#     print(key)
-
-# for value in numbers.values():
-#     print(value)
+for value in numbers.values():
+    print(value)
--- Python/Introduction/datatypes/sets.py
+++ Python/Introduction/datatypes/sets.py
@@ -3,11 +3,11 @@ a = {1,2,3,3,4,5,5}
 b = {4,'five',6}
 c = {'seven',8,9.0}
 
-#print(a[0])
-# print(type(a))
-# l = [1,2,3,3,4,5,5]
-# s = set(l)
-# print(s)
+#print(a[0]) unindexable(unordered)
+print(type(a))
+l = [1,2,3,3,4,5,5]
+s = set(l)
+print(s)
 
 s1 = set() #creating an empty set
 
@@ -20,16 +20,16 @@ print(s1)
 s1.update([4,5,6])
 print(s1)
 
-# s1.remove(7)
-# print(s1)
+s1.remove(6)
+print(s1)
 
-# s1.discard(20)
-# print(s1)
+s1.discard(20)
+print(s1)
 
-# s1.pop()
-# print(s1)
-# s1.clear()
-# print(s1)
+s1.pop()
+print(s1)
+s1.clear()
+print(s1)
 
-# for ele in s1:
-#     print(ele)
+for ele in s1:
+    print(ele)
