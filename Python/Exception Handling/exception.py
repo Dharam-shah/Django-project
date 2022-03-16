9f544eaf98997f14fe9d6c9eb445c91b36279b15
Dharam-shah <dharmendra11250@gmail.com>
2022-03-16 16:40:27 +0545

    adding exception handling filr

---
+++ Python/Exception Handling/exception.py
@@ -0,0 +1,13 @@
+# if 5 > 3:  # Syntax Error
+#     print("Hello") 
+
+# a = 5
+# print(b)  # Name Error
+
+#print(5/0)  # ZeroDivisionError
+#print(math.pi) # ImportError
+
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
